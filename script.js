1742300cf1e34a4228362b080cb15855d0a04837
sahaj7681 <65781649+sahaj7681@users.noreply.github.com>
2020-07-30 06:21:34 +0530

    Added Files

---
+++ script.js
@@ -0,0 +1,49 @@
+
+
+var st = document.getElementById('status');
+if(!navigator.geolocation) {
+    st.textContent = 'Geolocation is not supported by your browser';
+} else {
+   st.textContent = 'Locating…';
+   navigator.geolocation.getCurrentPosition(success, error);
+}
+function success(position) {
+    st.textContent = 'You Are at : Latitude = '+ position.coords.latitude + ', Longitude = '+position.coords.longitude;
+}
+
+function error() {
+    st.textContent = 'Unable to retrieve your location';
+}
+
+// XMLHttpRequest
+let request = new XMLHttpRequest();
+request.open('GET','https://sahaj7681.github.io/lab8/products.json');
+request.responseType = 'json';
+request.send();
+
+
+request.onload = function(){
+		let res = request.response
+		let dealsList = res.topDeals;
+		document.getElementById('title').textContent=res.companyName;
+		document.getElementById('address').textContent=res.headOffice;
+		document.getElementById('est').textContent=res.established;
+		let list = document.getElementById('weirdDeals');
+		for (let i = 0; i < dealsList.length; i++) {
+        let article = document.createElement('article');
+        let title = document.createElement('h3');
+        let image = document.createElement('img');
+        let price = document.createElement('p');
+        let features = document.createElement('p');
+        image.setAttribute('src', 'https://sahaj7681.github.io/lab8/images/' + dealsList[i].image);
+        title.textContent = dealsList[i].name;
+        price.textContent = '$'+dealsList[i].price;
+        features.textContent = dealsList[i].features;
+        article.appendChild(image);
+        article.appendChild(title);
+        article.appendChild(price);
+        article.appendChild(features);
+        list.appendChild(article);
+		}
+};
+
